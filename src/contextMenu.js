7db251a06031642f5c988be3f01b1296dff4705c
uyouthe <melvoloskov@ya.ru>
2018-08-16 20:52:39 +0300

    Ban toggle added

--- src/contextMenu.js
+++ src/contextMenu.js
@@ -7,25 +7,61 @@ chrome.runtime.onInstalled.addListener(() => {
 })
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === 'ban') {
-    chrome.storage.sync.get({
-      "bannedWords": []
-    }, items => {
-      const url = tab.url
-        .split('//')
-        .slice(1).join('')
-        .split('/')
-        .slice(0, 1).join('')
-        .split('.')
-        .slice(-2).join('.')
+  chrome.storage.sync.get({
+    bannedWords: []
+  }, items => {
+    chrome.storage.sync.set({
+      bannedWords: toggleArrItem(items.bannedWords, getDomainByUrl(tab.url))
+    })
+  })
+})
 
-      if (items.bannedWords.indexOf(url) === -1) {
-        chrome.storage.sync.set({
-          "bannedWords": items.bannedWords.concat([url])
-        })
-      }
+chrome.tabs.onActivated.addListener(updateCurrentDomainStatus)
+chrome.storage.onChanged.addListener(updateCurrentDomainStatus)
+
+
+function updateCurrentDomainStatus () {
+  chrome.tabs.query({
+    active: true
+  }, tabs => {
+    const domain = getDomainByUrl(tabs[0].url)
+
+    chrome.storage.sync.get({
+      bannedWords: []
+    }, items => {
+      chrome.contextMenus.update('ban', {
+        title: items.bannedWords.indexOf(domain) === -1 ?
+          'Ban this website' :
+          'Unban this website'
+      })
     })
 
-    chrome.tabs.remove(tab.id)
-  }
-})
+  })
+}
+
+function toggleArrItem(arr, item) {
+  return arr.indexOf(item) !== -1 ?
+    getArrWithout(arr, item) :
+    getArrWith(arr, item)
+}
+
+function getArrWithout (arr, value) {
+  const index = arr.indexOf(value)
+  return index !== -1 ?
+    arr.slice(0, index).concat(arr.slice(index + 1)) :
+    arr
+}
+
+function getArrWith (arr, value) {
+  return arr.concat([value])
+}
+
+function getDomainByUrl (url) {
+  return url
+    .split('//')
+    .slice(1).join('')
+    .split('/')
+    .slice(0, 1).join('')
+    .split('.')
+    .slice(-2).join('.')
+}
