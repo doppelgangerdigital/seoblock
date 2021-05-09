67940749a9649527368d3a23a7313306de6b41ff
mvoloskov <hello@miloslav.website>
2021-05-09 16:34:45 +0300

    Revert "done"
    This reverts commit a1797f5b4994c456eba7bd42f15f1f8aee19f5e4.

--- src/app.js
+++ src/app.js
@@ -1,76 +1,54 @@
-const wrapperSelector = `.ListingLayout-outerContainer > *:nth-child(2) > *:nth-child(3) > *:first-child`
-const parentSelector = `.Post`
-const tagSelector = `[data-click-id="subreddit"]`
+const placeholder = 'This result was hidden due to your SeoBlock preferences.'
 
-const debounce = (fn, wait = 100) => {
-    // this is double debounce
-    // it fires like this
-    // calls:          1111111111111111
-    // actually fired: 1000000000000001
-
-    // so only first and last call
-
-    let shouldCallMore
-    let timer
-    let maxWaitTimer
-    let isWaiting
-    let fnArgs = []
-
-    const stopWaiting = () => {
-        if (shouldCallMore) fn(...fnArgs)
-        isWaiting = shouldCallMore = false
-    }
-
-    const call = () => {
-        shouldCallMore = false
-        fn(...fnArgs)
-    }
-
-    return (...args) => {
-        fnArgs = args
-
-        shouldCallMore = true
-
-        clearTimeout(timer)
-        timer = setTimeout(stopWaiting, wait)
-
-        if (isWaiting) return
-        isWaiting = true
-
-        clearTimeout(maxWaitTimer)
-        maxWaitTimer = setTimeout(call, 20)
-    }
-}
-
-let observer
-
-const start = () => {
-    if (observer) return
-
-    const handleChanges = () => debounce(chrome.storage.sync.get(null, store => {
-        const bannedTags = store.bannedWords || []
-        if (bannedTags.length === 0) return
-        console.log(bannedTags)
-        document.querySelectorAll(parentSelector).forEach(parent => {
-            const tag = parent.querySelector(tagSelector)?.getAttribute('href')
-                .replace('r/', '')
-                .replaceAll('/', '')
-
-            console.log(parent, parent.querySelector(tagSelector), tag)
-            if (!bannedTags.includes(tag) && !bannedTags.includes('r/' + tag)) return
-            parent.remove()
-        })
-    }))
-
-    handleChanges()
-    observer = new MutationObserver(handleChanges)
-    observer.observe(document.querySelector(wrapperSelector), {
-        childList: true,
-    })
-}
-
-if (document.readyState === 'interactive' || document.readyState === 'complete') {
-    start()
+if (document.readyState === 'interactive') {
+  start()
 } else {
-    document.addEventListener('DOMContentLoaded', start)
+  document.addEventListener('DOMContentLoaded', start)
+}
+
+function start () {
+  getFilters(filters => {
+    loadCurrentDomainFilters(filters, currentDomainFilters => {
+      chrome.storage.sync.get(null, items => {
+        toArray(document.querySelectorAll(currentDomainFilters)).forEach(element => {
+          inspectElement(element, items.bannedWords)
+        })
+      })
+    })
+  })
+}
+
+function inspectElement (element, bannedWords) {
+  const content = extractContent(element).toLowerCase()
+
+  bannedWords.forEach(word => {
+    if (content.indexOf(word.toLowerCase()) !== -1) {
+      element.innerHTML = placeholder
+    }
+  })
+}
+
+function extractContent (element) {
+  return element.textContent +
+    element.getAttribute('href') || ''
+}
+
+function toArray(DOMList) {
+  return Array.prototype.slice.call(DOMList)
+}
+
+function loadCurrentDomainFilters (filters, cb) {
+  const domains = Object.keys(filters)
+  domains.forEach(domain => {
+    if (window.location.hostname.indexOf(domain) !== -1) {
+      cb(filters[domain])
+      return
+    }
+  })
+}
+
+function getFilters (cb) {
+  fetch("https://cdn.rawgit.com/uyouthe/seoblock/master/src/filters.json")
+    .then(response => response.json())
+    .then(cb)
 }
